f5c683e9cbc707d174cc8494d0d2c784c4088c48
Rob Holme <robert.holme@health.wa.gov.au>
2020-01-13 16:49:54 +0800

    report status codes caught by exception handler (401s, 404s, etc)

--- public/Connect-URL.psm1
+++ public/Connect-URL.psm1
@@ -25,23 +25,35 @@ The URL to connect to.
 		[string] $URL
 	)
 
-	$request = [System.Net.WebRequest]::Create($URL)
-	try {
-		$timeElapsed = Measure-Command {
-			$response = $request.GetResponse()
+	process {
+
+		$status = ""
+		$request = [System.Net.WebRequest]::Create($URL)
+		try {
+			$timeElapsed = Measure-Command {
+				$response = $request.GetResponse()
+			}
+			$status = [int]$response.StatusCode
+			$response.Close()
 		}
-		$status = [int]$response.StatusCode
+		# catch exceptions thrown (401's 404's etc). 
+		# Note: Some sites redirect page not found to a custom page via a 302 - these would be mistakenly reported as a 200.
+		catch {
+			write-warning "Failed to connect to $URL.  $($_.Exception.Message)"
+			if ($_.Exception.Message -match "(?<=\()\d{3}") {
+				$status = $Matches[0]
+			}
+			else {	
+				Write-Error $_.Exception.Message
+			}
+		}
 		$result = [Ordered]@{
 			StatusCode   = $status
 			ResponseTime = "$([math]::Round($timeElapsed.Milliseconds,2)) (ms)"  
 			URL          = $URL
 		} 
-		$response.Close()
 		$outputObject = New-Object -Property $result -TypeName psobject
 		$outputObject.PSObject.TypeNames.Insert(0, "Powertools.ConnectURL.Result")
 		Write-Output $outputObject
-	}
-	catch {
-		write-warning "Failed to connect to $URL"
 	}
 }
