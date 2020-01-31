b9c91fc4e1427f83145f14cd5c53f80e42b25c5d
Rob Holme <robert.holme@health.wa.gov.au>
2020-01-31 10:11:48 +0800

    error changed to warning on exception handler

--- public/Connect-URL.psm1
+++ public/Connect-URL.psm1
@@ -38,13 +38,12 @@ The URL to connect to.
 		}
 		# catch exceptions thrown (401's 404's etc). 
 		# Note: Some sites redirect page not found to a custom page via a 302 - these would be mistakenly reported as a 200.
-		catch {
-			write-warning "Failed to connect to $URL.  $($_.Exception.Message)"
+		catch {	
 			if ($_.Exception.Message -match "(?<=\()\d{3}") {
 				$status = $Matches[0]
 			}
 			else {	
-				Write-Error $_.Exception.Message
+				write-warning "Failed to connect to $URL.  $($_.Exception.Message)"
 			}
 		}
 		$result = [Ordered]@{
