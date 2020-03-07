613a34670214409f6f08353b140c5ea6b50517d8
Rob Holme <rob@holme.com.au>
2020-03-07 12:57:12 +0800

    module now dot sources functions - faster than nested modules

--- PowerTools.psm1
+++ PowerTools.psm1
@@ -1,8 +1,11 @@
 # Dot source public/private functions
-$publicFunctionsPath = Join-Path -Path $PSScriptRoot -ChildPath 'public/*.ps1' 
-$privateFunctionsPath = Join-Path -Path $PSScriptRoot -ChildPath 'private/*.ps1'
-$public = @(Get-ChildItem -Path $publicFunctionPath -Recurse -ErrorAction Stop) 
-$private = @(Get-ChildItem -Path $privateFunctionPath -Recurse -ErrorAction Stop) 
+
+$publicFunctionsPath = Join-Path -Path (Split-Path $script:MyInvocation.MyCommand.Path) -ChildPath '\public\*.ps1' 
+$privateFunctionsPath = Join-Path -Path (Split-Path $script:MyInvocation.MyCommand.Path) -ChildPath '\private\*.ps1'
+
+Write-Verbose $publicFunctionsPath
+$public = @(Get-ChildItem -Path $publicFunctionsPath -Recurse -ErrorAction Stop) 
+$private = @(Get-ChildItem -Path $privateFunctionsPath -Recurse -ErrorAction Stop) 
 foreach ($file in @($public + $private)) {
 	try { 
 		. $file.FullName 
