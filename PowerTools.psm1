5a7e10543cd9c0dfd5dd1f794cce4947ba9e5e5e
Rob Holme <rob@holme.com.au>
2020-03-07 16:18:16 +0800

    implemented recomendations from PSCriptAnalyser

--- PowerTools.psm1
+++ PowerTools.psm1
@@ -1,22 +1,22 @@
 # Dot source public/private functions
 
-$publicFunctionsPath = Join-Path -Path (Split-Path $script:MyInvocation.MyCommand.Path) -ChildPath '\public\*.ps1' 
+$publicFunctionsPath = Join-Path -Path (Split-Path $script:MyInvocation.MyCommand.Path) -ChildPath '\public\*.ps1'
 $privateFunctionsPath = Join-Path -Path (Split-Path $script:MyInvocation.MyCommand.Path) -ChildPath '\private\*.ps1'
 $public = @()
 $private = @()
 
 # test that the public and private function paths exist, if so load all scripts found.
 if (test-path $publicFunctionsPath) {
-	$public = @(Get-ChildItem -Path $publicFunctionsPath -Recurse -ErrorAction Stop) 
+	$public = @(Get-ChildItem -Path $publicFunctionsPath -Recurse -ErrorAction Stop)
 }
 if (Test-Path $privateFunctionsPath) {
-	$private = @(Get-ChildItem -Path $privateFunctionsPath -Recurse -ErrorAction Stop) 
+	$private = @(Get-ChildItem -Path $privateFunctionsPath -Recurse -ErrorAction Stop)
 }
 foreach ($file in @($public + $private)) {
-	try { 
-		. $file.FullName 
+	try {
+		. $file.FullName
 	}
 	catch {
-		throw "Unable to dot source [$($file.FullName)]" 
+		throw "Unable to dot source [$($file.FullName)]"
 	}
 }
