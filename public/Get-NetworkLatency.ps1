bd5c79aeeb474c77fbb8663d1f1d787d2755d2c1
Rob Holme <rob@holme.com.au>
2023-02-19 16:07:16 +0800

    If an array of hosts is supplied for the Hostname paramter they will be tested in parallel

--- public/Get-NetworkLatency.ps1
+++ public/Get-NetworkLatency.ps1
@@ -6,7 +6,7 @@ function Get-NetworkLatency {
 			Mandatory = $true,
 			ValueFromPipeline = $true
 		)]
-		[string] $Hostname,
+		[string[]] $Hostname,
 
 		[Parameter(
 			Position = 1,
@@ -39,13 +39,15 @@ function Get-NetworkLatency {
 		[string] $GraphResult
 	)	
 
+	
+	#--------------------------------------
+	# One off actions 
 	begin {
-
 		# timeout defaults to 4 seconds. Make the timeout the same as the interval if the interval is less than 4 seconds
 		[int] $timeout = if ($Interval -lt 4) { $Interval } else { 4 }
 
-		[LatencyResultCollection] $resultList = New-Object LatencyResultCollection
-
+		[LatencyResultCollection] $latencyResultList = [LatencyResultCollection]::new()
+		
 		# Load the ScottPlot library if outputting the result as a graph
 		if ($GraphResult) {
 			try {
@@ -58,33 +60,37 @@ function Get-NetworkLatency {
 		}
 	}
 	
+	#--------------------------------------
+	# process each object from the pipeline
 	process {
 		# Catch hostname resolution failure. Move onto next host in the input pipeline.
-		try {
-			[System.Net.Dns]::GetHostaddresses($Hostname) | Out-Null
+		foreach ($currentHost in $Hostname) {
+			try {
+				$resolvedIP = [System.Net.Dns]::GetHostaddresses($currentHost) 
+				Write-Verbose "$currentHost resolved to $($resolvedIP.IPAddressToString)"
+			}
+			catch {
+				Write-Error "Unable to resolve the Hostname: $currentHost"
+				return
+			}
 		}
-		catch {
-			Write-Warning "Unable to resolve the Hostname: $Hostname"
-			return
-		}
-
-		# Clear summary values from prior hosts (from the input pipeline). Keep individual results for charting etc.
-		$resultList.ResetSummary()
 
 		for ($i = 0; $i -lt $Count; $i++) {
 			[int] $secondsRemaining = ($Count - $i) * $Interval
 			[int] $percentComplete = (($i + 1) / $Count) * 100
-			Write-Progress -Activity "Testing $Hostname" -SecondsRemaining $secondsRemaining -PercentComplete $percentComplete -Status "Measuring network latency ..."    
-			$latencyResult = TestLatency -DestinationHost $Hostname -Timeout $timeout
-			$resultList.AddResult($latencyResult)	
-			# show all results as they are returned if -ShowAllResults switch is set 
-			if ($ShowAllResults) {
-				[PSCustomObject]@{
-					PSTypeName  = "Powertools.GetNetworkLatency.ImmediateResult"
-					Destination = $Hostname
-					Latency     = "$($latencyResult.Latency) ms"
-					Status      = $latencyResult.Status
-					TimeStamp   = $latencyResult.TimeStamp.ToString("yyyy-MM-dd HH:mm:ss")
+			foreach ($currentHost in $Hostname) {
+				Write-Progress -SecondsRemaining $secondsRemaining -PercentComplete $percentComplete -Activity "Measuring network latency ..."    
+				$latencyResult = TestLatency -DestinationHost $currentHost -Timeout $timeout
+				$latencyResultList.AddResult($latencyResult)	
+				# show all results as they are returned if -ShowAllResults switch is set 
+				if ($ShowAllResults) {
+					[PSCustomObject]@{
+						PSTypeName  = "Powertools.GetNetworkLatency.ImmediateResult"
+						Destination = $currentHost
+						Latency     = "$($latencyResult.Latency) ms"
+						Status      = $latencyResult.Status
+						TimeStamp   = $latencyResult.TimeStamp.ToString("yyyy-MM-dd HH:mm:ss")
+					}
 				}
 			}
 			# don't sleep in the last iteration, otherwise sleep based on the $Interval parameter
@@ -94,121 +100,38 @@ function Get-NetworkLatency {
 		}
 		# show summary of results for the current destination if -ShowAllResults switch not set
 		if (!$ShowAllResults) {
-			$failedPercent = if ($resultList.TotalCount -gt 0) { [Math]::Round(($resultList.FailedCount / $resultList.TotalCount) * 100) }
-			[PSCustomObject]@{
-				PSTypeName  = "Powertools.GetNetworkLatency.SummaryResult"
-				Destination = $Hostname
-				Average     = "$($resultList.AverageLatency) ms"
-				Minimum     = "$($resultList.MinLatency) ms"
-				Maximum     = "$($resultList.MaxLatency) ms"
-				Loss        = "$($resultList.FailedCount) ($failedPercent%)"
+			foreach ($currentHost in $Hostname) {
+				$failedPercent = if ($latencyResultList.TotalCount($currentHost) -gt 0) { [Math]::Round(($latencyResultList.FailedCount($currentHost) / $latencyResultList.TotalCount($currentHost)) * 100) }
+				[PSCustomObject]@{
+					PSTypeName  = "Powertools.GetNetworkLatency.SummaryResult"
+					Destination = $currentHost
+					Average     = "$($latencyResultList.AverageLatency($currentHost)) ms"
+					Minimum     = "$($latencyResultList.MinLatency($currentHost)) ms"
+					Maximum     = "$($latencyResultList.MaxLatency($currentHost)) ms"
+					Loss        = "$($latencyResultList.FailedCount($currentHost)) ($failedPercent%)"
+				}
 			}
 		}
 	}
 
+	
+	#--------------------------------------
+	# Final actions after process() has finished for all object from the pipeline
 	end {
-		# create graph
-		if (!$noGraph) {
-			GenerateChart -LatencyResults $resultList -Filename $GraphResult
+		if ((!$noGraph) -and ($GraphResult)) {
+			$allResultsHash = GetLatencyHashTable -LatencyResults $latencyResultList
+			if ($Count -gt 50) {
+				GenerateChart -LatencyHashtable $allResultsHash -Filename $GraphResult -Height 900 -Width 1920
+			}
+			else {
+				GenerateChart -LatencyHashtable $allResultsHash -Filename $GraphResult -Height 900 -Width 1200
+			}
 		}
 	}
 }
 
 
-# A class containing a collection of LatencyResult objects. Summery latency results (min, max, average, num failures) from the total collection are provided.
-class LatencyResultCollection {
-	[Collections.Generic.List[LatencyResult]] $resultList
-	[int] $totalCount
-	[int] $failedCount
-	[int] $maxLatency
-	[int] $minLatency
-	[int] $sumLatency
-	[int] $averageLatency
-
-	LatencyResultCollection () {
-		$this.resultList = New-Object Collections.Generic.List[LatencyResult]
-		$this.totalCount = 0
-		$this.failedCount = 0
-		$this.maxLatency = 0
-		$this.minLatency = 0
-		$this.sumLatency = 0
-		$this.averageLatency = 0
-	}
-
-	# Add a LatencyResult object to the collection. Recalculate max, min and average properties
-	AddResult ([LatencyResult] $Result) {
-		# return if the LatencyResult object received is $null
-		if ($null -eq $Result) {
-			Write-Verbose "Null LatencyResult object received. Excluding from results."
-			return
-		}
-
-		$this.resultList.Add($Result)
-		$this.totalCount++
-		if ($Result.Status -ne "Success") {
-			$this.failedCount++
-			Write-Warning "Connection timeout"
-		}
-		# if a latency value was returned, recalculate the max,min, and average properties
-		else {
-			# average latency
-			$this.sumLatency += $Result.Latency
-			if (($this.totalCount - $this.failedCount) -gt 0) {
-				$this.averageLatency = ($this.sumLatency) / ($this.totalCount - $this.failedCount)
-			}
-			# max latency
-			if ($Result.Latency -gt $this.maxLatency) {
-				$this.maxLatency = $Result.Latency
-			}
-			# min latency (if min latency is zero, then use this value for the min latency)
-			if (($Result.Latency -lt $this.minLatency) -or ($this.minLatency -eq 0) ) {
-				$this.minLatency = $Result.Latency
-			}
-		}
-	}
-
-	# Get the collection of results
-	[Collections.Generic.List[LatencyResult]] GetResults() {
-		return $this.resultList
-	}
-
-	# Get the Max Latency value from all results
-	[int] MaxLatency() {
-		return $this.maxLatency
-	}
-
-	# Get the Min latency value from all results
-	[int] MinLatency() {
-		return $this.minLatency
-	}
-
-	# get the total number of results
-	[int] TotalCount() {
-		return $this.totalCount
-	}
-
-	# Get the number os failed/timed out results
-	[int] FailedCount() {
-		return $this.failedCount
-	}
-
-	# Get the average latency for all successful results (failed results omitted)
-	[int] AverageLatency() {
-		return $this.averageLatency
-	}
-
-	# reset the summary values between hosts
-	ResetSummary() {
-		$this.totalCount = 0
-		$this.failedCount = 0
-		$this.maxLatency = 0
-		$this.minLatency = 0
-		$this.sumLatency = 0
-		$this.averageLatency = 0
-	}
-}
-
-
+#--------------------------------------
 # Stores the result of a single ICMP test to measure the network latency to a remote host
 class LatencyResult {
 	[string] $source
@@ -220,9 +143,9 @@ class LatencyResult {
 	LatencyResult([string] $Source, [string] $Destination, [int] $Latency, [string] $Status, [datetime] $TimeStamp) {
 		$this.source = $Source
 		$this.destination = $Destination
-		$this.latency = $Latency
 		$this.status = $Status
 		$this.timeStamp = $TimeStamp
+		$this.latency = $Latency
 	}
 
 	# Get the source host 
@@ -251,7 +174,148 @@ class LatencyResult {
 	}
 }
 
-# Test the latency using Test-Connection. Return a LatencyResult object.
+
+#--------------------------------------
+# A class containing a collection of LatencyResult objects. 
+# Summery latency results (min, max, average, num failures) calculated for each destination host are exposed as members
+class LatencyResultCollection {
+	[Collections.Generic.List[LatencyResult]] $resultList
+	[hashtable] $totalCountTable = @{}
+	[hashtable] $failedCountTable = @{}
+	[hashtable] $maxLatencyTable = @{}
+	[hashtable] $minLatencyTable = @{}
+	[hashtable] $sumLatencyTable = @{}
+	[hashtable] $averageLatencyTable = @{}
+
+	LatencyResultCollection () {
+		$this.resultList = New-Object Collections.Generic.List[LatencyResult]
+	}
+
+	# Add a LatencyResult object to the collection. Recalculate max, min and average properties
+	[void] AddResult ([LatencyResult] $Result) {
+		# return if the LatencyResult object received is $null
+		if ($null -eq $Result) {
+			Write-Verbose "Null LatencyResult object received. Excluding from results."
+			return
+		}
+		$this.resultList.Add($Result)
+
+		# increment the total count for the destination
+		if ($this.totalCountTable.ContainsKey($Result.Destination)) {
+			$this.totalCountTable[$Result.Destination]++
+		}
+		else {
+			$this.totalCountTable.Add($Result.Destination, 1)
+		}
+
+		# increment the failed count for the destination
+		if ($Result.Status -ne "Success") {
+			if ($this.failedCountTable.ContainsKey($Result.Destination)) {
+				$this.failedCountTable[$Result.Destination]++
+			}
+			else {
+				$this.failedCountTable.Add($Result.Destination, 1)
+			}
+			Write-Warning "Connection timeout to $($Result.Destination)"
+		}
+
+		# if a latency value was returned, recalculate the max, min, and average properties
+		else {
+			# calculate average latency (for the destination host)
+			if ($this.sumLatencyTable.ContainsKey($Result.Destination)) {
+				$this.sumLatencyTable[$Result.Destination] += $Result.Latency
+			}
+			else {
+				$this.sumLatencyTable.Add($Result.Destination, $Result.Latency)
+			}
+
+			if (($this.totalCountTable[$Result.Destination] - $this.failedCountTable[$Result.Destination]) -gt 0) {
+				$this.averageLatencyTable[$Result.Destination] = ($this.sumLatencyTable[$Result.Destination]) / ($this.totalCountTable[$Result.Destination] - $this.failedCountTable[$Result.Destination])
+			}
+
+			# calculate max latency (for the destination host)
+			if ($this.maxLatencyTable.ContainsKey($Result.Destination)) {
+				if ($Result.Latency -gt $this.maxLatencyTable[$Result.Destination]) {
+					$this.maxLatencyTable[$Result.Destination] = $Result.Latency
+				}
+			}
+			else {
+				$this.maxLatencyTable.Add($Result.Destination, $Result.Latency)
+			}
+
+			# calculate min latency (for the destination host)
+			if ($this.minLatencyTable.ContainsKey($Result.Destination)) {
+				if ($Result.Latency -lt $this.minLatencyTable[$Result.Destination]) {
+					$this.minLatencyTable[$Result.Destination] = $Result.Latency
+				}
+			}
+			else {
+				$this.minLatencyTable.Add($Result.Destination, $Result.Latency)
+			}
+		}
+	}
+
+	# Get the collection of results
+	[Collections.Generic.List[LatencyResult]] GetResults() {
+		return $this.resultList
+	}
+
+	# Get the Max Latency value from all results
+	[int] MaxLatency([string] $DestinationHost) {
+		if ($this.maxLatencyTable.ContainsKey($DestinationHost)) {
+			return $this.maxLatencyTable[$DestinationHost]
+		}
+		else {
+			return 0
+		}
+	}
+
+	# Get the Min latency value from all results
+	[int] MinLatency([string] $DestinationHost) {
+		if ($this.minLatencyTable.ContainsKey($DestinationHost)) {
+			return $this.minLatencyTable[$DestinationHost]
+		}
+		else {
+			return 0
+		}
+	}
+
+	# get the total number of results
+	[int] TotalCount([string] $DestinationHost) {
+		if ($this.totalCountTable.ContainsKey($DestinationHost)) {
+			return $this.totalCountTable[$DestinationHost]
+		}
+		else {
+			return 0
+		}
+	}
+
+	# Get the number os failed/timed out results
+	[int] FailedCount([string] $DestinationHost) {
+		if ($this.failedCountTable.ContainsKey($DestinationHost)) {
+			return $this.failedCountTable[$DestinationHost]
+		}
+		else {
+			return 0
+		}
+	}
+
+	# Get the average latency for all successful results (failed results omitted)
+	[int] AverageLatency([string] $DestinationHost) {
+		if ($this.averageLatencyTable.ContainsKey($DestinationHost)) {
+			return $this.averageLatencyTable[$DestinationHost]
+		}
+		else {
+			return 0
+		}
+	}
+
+}
+
+
+#--------------------------------------
+# Test the latency using Test-Connection. 
+# Return a LatencyResult object.
 function TestLatency() {
 	# Return type
 	[OutputType([LatencyResult])]
@@ -267,54 +331,90 @@ function TestLatency() {
 
 	try {
 		$result = Test-Connection -TargetName $DestinationHost -Count 1  -TimeoutSeconds $Timeout
-		return New-Object LatencyResult($result.Source, $Hostname, $result.Latency, $result.Status, [datetime]::Now)
+		return New-Object LatencyResult($result.Source, $DestinationHost, $result.Latency, $result.Status, [datetime]::Now)
 	}
-	catch [System.Exception] {
-		Write-error $_.Exception.Message
+	catch {
+		Write-Warning $_.Exception.Message
 		Write-Debug $_.Exception
 		return $null
 	}
 }
 
-# Generate a signal graph of latency results for each host.
+
+#--------------------------------------
+# Generate a signal chart (signal plot) of latency results for each host.
 function GenerateChart {
 	Param (
 		[parameter(Mandatory = $true)]
-		[LatencyResultCollection] $LatencyResults,
+		[Hashtable] $LatencyHashtable,
 
 		[parameter(Mandatory = $true)]
-		[string] $Filename
+		[string] $Filename,
+
+		[parameter(Mandatory = $false)]
+		[int] $Width = 1200,
+
+		[parameter(Mandatory = $false)]
+		[int] $Height = 900
 	)
 
 	if (!$noGraph) {
-		$latencyTable = @{}
-		foreach ($result in $LatencyResults.resultList) {
-			if ($latencyTable.ContainsKey($result.Destination)) {
-				# only plot successful results
-				if ($result.Status -eq "Success") {
-					$latencyTable[$result.destination] += $result.Latency
-				}
+		$chart = [ScottPlot.Plot]::new($Width, $Height)
+		foreach ($key in $LatencyHashtable.Keys) {
+			$plotLine = $chart.AddSignal($LatencyHashtable[$key], 1, $null, $key.ToString())
+			$plotLine.LineWidth = 3
+			$plotLine.MarkerSize = 7
+			$plotLine.MarkerShape = [ScottPlot.MarkerShape]::filledDiamond
+			$plotLine.Smooth = $true
+			# tighten the smoothing tension if more than 50 samples
+			if ($LatencyHashtable[$key].Count -lt 50) {
+				$plotLine.SmoothTension = 0.4
 			}
 			else {
-				$latencyTable[$result.destination] = @($result.Latency)
+				$plotLine.SmoothTension = 0.2
 			}
-		}
-
-		$chart = [ScottPlot.Plot]::new(1200, 900)
-		foreach ($key in $latencyTable.Keys) {
-			[void] $chart.AddSignal($latencyTable[$key],1,$null,$key.ToString())
 		} 
 
-		[void] $chart.Legend($true, [ScottPlot.Alignment]::UpperLeft)
-		$chart.Title("Network Latency",$null)
+		# set chart legend and axis labels
+		$legend = $chart.Legend($true, [ScottPlot.Alignment]::UpperLeft)
+		$legend.Orientation = [ScottPlot.Orientation]::Horizontal
+		$chart.Title("Network Latency", $null)
 		$chart.YLabel("Latency (ms)")
 		$chart.XLabel("ICMP Requests")
 
 		# Save the chart. If relative path provided base the location on hte current directory.
 		[System.IO.Directory]::SetCurrentDirectory($(get-location))
 		$Filename = [System.IO.Path]::GetFullPath($Filename)
-		$chartFilename = $chart.SaveFig($Filename)
-		Write-Host "Chart saved to $chartFilename" -ForegroundColor green
+		try {
+			$chartFilename = $chart.SaveFig($Filename)
+			Write-Host "Chart saved to $chartFilename" -ForegroundColor green
+			& $chartFilename
+		}
+		catch {
+			Write-Warning "Unable to save chart to $Filename"
+			Write-Warning $_.Exception.Message
+			Write-Debug $_.Exception
+		}
 	}
 }
 
+
+#--------------------------------------
+# Formats the latency result collection as a hash table. The Hostname is used as the table key.
+function GetLatencyHashTable() {
+	Param (
+		[parameter(Mandatory = $true)]
+		[LatencyResultCollection] $LatencyResults
+	)
+
+	$latencyTable = @{}
+	foreach ($result in $LatencyResults.resultList) {
+		if ($latencyTable.ContainsKey($result.Destination)) {
+			$latencyTable[$result.destination] += $result.Latency
+		}
+		else {
+			$latencyTable[$result.destination] = @($result.Latency)
+		}
+	}
+	return $latencyTable
+}
