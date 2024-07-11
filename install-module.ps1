4bc9bdca206c67cc8fd6451961cf4d742767fc73
Rob Holme <rob@holme.com.au>
2024-07-11 19:45:10 +0800

    continued work on install script - not finished

--- install-module.ps1
+++ install-module.ps1
@@ -18,50 +18,50 @@ param (
     [switch] $NoClobber
 )
 
-begin {  
-    function Get-ModuleVersion() {
-        try {
-            $moduleFile = Get-ChildItem *.psd1
-        }
-        catch {
-            Write-Error "Exception raised while detecting module file, exiting. Use -Debug switch to view exception" 
-            Write-Debug $_.Exception
-            return $null
-        }
+
+function Get-ModuleVersion() {
+    try {
+        $moduleFile = Get-ChildItem *.psd1
+    }
+    catch {
+        Write-Error "Exception raised while detecting module file, exiting. Use -Debug switch to view exception" 
+        Write-Debug $_.Exception
+        return $null
+    }
         
-        # Make sure only one module file is found, otherwise exit.
-        if ($moduleFile.Count -eq 1) {
-            $versionElement = Get-Content $moduleFile | Select-String "ModuleVersion(\s){0,}=(\s){0,}('|"")\d{1,}(\.{1}\d{1,}){0,}"
-            if ($versionElement.Matches.Count -ne 1) {
-                Write-Error "ModuleVersion element not detected in manifest"
-                return $null
-            }
-            else {
-                # match version number string
-                $moduleVersionMatch = ([regex]::Match($versionElement,"\d{1,}(\.{1}\d{1,}){0,}"))
-                if (moduleVersionMatch.Success) {
-                    return $moduleVersionMatch[0].Value
-                }
-                else {
-                    return $null
-                }
-            }
-        } 
-        elseif ($moduleFile.Count -eq 0) {
-            Write-Error "Module .psd file not found, exiting."
-            return $null
-        } 
-        elseif ($moduleFile.Count -gt 1) {
-            Write-Error "More than 1 .psd file found, exiting."
+    # Make sure only one module file is found, otherwise exit.
+    if ($moduleFile.Count -eq 1) {
+        $versionElement = Get-Content $moduleFile | Select-String "ModuleVersion(\s){0,}=(\s){0,}('|"")\d{1,}(\.{1}\d{1,}){0,}"
+        if ($versionElement.Matches.Count -ne 1) {
+            Write-Error "ModuleVersion element not detected in manifest"
             return $null
         }
         else {
-            Write-Error "Unknown issue detecting module file, exiting." 
-            return $null
+            # match version number string
+            $moduleVersionMatch = ([regex]::Match($versionElement, "\d{1,}(\.{1}\d{1,}){0,}"))
+            if (moduleVersionMatch.Success) {
+                return $moduleVersionMatch[0].Value
+            }
+            else {
+                return $null
+            }
         }
+    } 
+    elseif ($moduleFile.Count -eq 0) {
+        Write-Error "Module .psd file not found, exiting."
+        return $null
+    } 
+    elseif ($moduleFile.Count -gt 1) {
+        Write-Error "More than 1 .psd file found, exiting."
+        return $null
     }
+    else {
+        Write-Error "Unknown issue detecting module file, exiting." 
+        return $null
+    }
+}
 
-    # Get the module path based on scope and platform
+# Get the module path based on scope and platform
 function Get-PSModulePath {
     param (
         [ValidateSet("CurrentUser", "AllUsers")]
@@ -76,8 +76,8 @@ function Get-PSModulePath {
         else { 
             "WindowsPowerShell" 
         }
-            $localUserDir = Join-Path ([Environment]::GetFolderPath([Environment+SpecialFolder]::MyDocuments)) $powerShellType
-            $allUsersDir = Join-Path ([Environment]::GetFolderPath([Environment+SpecialFolder]::ProgramFiles)) $powerShellType
+        $localUserDir = Join-Path ([Environment]::GetFolderPath([Environment+SpecialFolder]::MyDocuments)) $powerShellType
+        $allUsersDir = Join-Path ([Environment]::GetFolderPath([Environment+SpecialFolder]::ProgramFiles)) $powerShellType
         
     }
     else {
@@ -112,55 +112,15 @@ function Get-HomeOrCreateTempHome {
 }
 
 
+
+$moduleVersion = Get-ModuleVersion
+$moduleRootPath = Get-PSModulePath
+if ($null -ne $moduleVersion) {
+    # copy all files, exclude .git folder
+    # Copy-Item -Path -Destination -Recurse -Exclude '.*'
+}
     
-}
-
-process {
-    $moduleVersion = Get-ModuleVersion
-    $moduleRootPath =  Get-HomeOrCreateTempHome
-    if ($null -ne $moduleVersion){
-        # copy all files, exclude .git folder
-        # Copy-Item -Path -Destination -Recurse -Exclude '.*'
-    }
-    
-}
 
 
-
-
-
-# Example usage
-#$localUserDir = ""
-#$allUsersDir = ""
-#Get-StandardPlatformPaths -psCmdlet $MyInvocation.MyCommand -localUserDir ([ref] $localUserDir) -allUsersDir ([ref] $allUsersDir)
-#Write-Host "Local User Dir: $localUserDir"
-#Write-Host "All Users Dir: $allUsersDir"
-
-<#
-
-private static void GetStandardPlatformPaths(
-    PSCmdlet psCmdlet,
-    out string localUserDir,
-    out string allUsersDir)
-{
-    if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-    {
-        string powerShellType = (psCmdlet.Host.Version >= PSVersion6) ? "PowerShell" : "WindowsPowerShell";
-        localUserDir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), powerShellType);
-        allUsersDir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles), powerShellType);
-    }
-    else
-    {
-        // paths are the same for both Linux and macOS
-        localUserDir = Path.Combine(GetHomeOrCreateTempHome(), ".local", "share", "powershell");
-        // Create the default data directory if it doesn't exist.
-        if (!Directory.Exists(localUserDir)) {
-            Directory.CreateDirectory(localUserDir);
-        }
-
-        allUsersDir = System.IO.Path.Combine("/usr", "local", "share", "powershell");
-    }
-}
-#>
 
 
