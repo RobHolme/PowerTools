e4454eaa09889ee7eddc08d71a12f9289a703b68
Rob Holme <rob@holme.com.au>
2024-07-15 18:49:20 +0800

    Fixed issue with -Scope parameter being overwritten

--- install.ps1
+++ install.ps1
@@ -159,7 +159,7 @@ function Get-ModuleName {
 
 # Prompt user to select modile if "PromptForModulePath" provided as the -Scope parameter, otherwise use AllUsers or CurrentUser path based on the parameter 
 # Note: -Scope defaults to CurrentUser if no paramter value provided. 
-if($scope = "PromptForModulePath") {
+if($scope -eq "PromptForModulePath") {
     $moduleRootPath = Select-PSModulePath
 }
 else {
